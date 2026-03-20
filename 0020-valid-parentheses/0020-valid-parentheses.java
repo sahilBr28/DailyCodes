140be45cf384cd244a479b8a032ed537356f6ed4
sahilBr28 <sahilkumar0092004@gmail.com>
2026-03-20 18:24:58 +0530

    Time: 4 ms (41.53%), Space: 43.4 MB (52.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n%2 == 1){
+            return false;
+        }
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{') st.push(ch);
+            else{
+                if(st.size()==0) return false;
+                char top = st.peek();
+                if(ch==')' && top=='(') st.pop();
+                else if(ch==']' && top=='[') st.pop();
+                else if(ch=='}' && top=='{') st.pop();
+                else return false;
+                
+            }
+        }
+        return (st.size()==0);
+    }
+}
