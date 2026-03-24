24b15300e985d09576003df6762dbba31ac23b56
sahilBr28 <sahilkumar0092004@gmail.com>
2026-03-24 14:12:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,22 +1,22 @@
 class Solution {
     public String reverseWords(String s) {
         int n = s.length();
-        String ans = "";
-        String temp = "";
-
+        StringBuilder ans = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+    
         for(int i=0;i<=n-1;i++){
             if(i>0 && s.charAt(i)==' ' && s.charAt(i-1)==' '){
                 continue;
             }
             if(s.charAt(i)==' '){
-                ans = temp + ans;
-                temp = "";
-                ans = " " + ans;
+                ans.insert(0, temp.toString());
+                temp.setLength(0);  
+                ans.insert(0, " ");
             }else{
-                temp += s.charAt(i);
+                temp.append(s.charAt(i));   
             }
         }
-        ans = temp + ans;
-        return ans.trim();
+        ans.insert(0, temp.toString());
+        return ans.toString().trim();
     }
 }
