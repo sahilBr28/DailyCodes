f029260736b7dcc7f530211ee96d10d32dfc1510
sahilBr28 <sahilkumar0092004@gmail.com>
2026-03-26 16:50:34 +0530

    Time: 0 ms (100%), Space: 42.6 MB (82.07%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,6 +3,7 @@ class Solution {
         int n = s.length();
         int m = goal.length();
 
+        if(n != m) return false;
         String ans = s + s;
         if(ans.contains(goal)) return true;
         else return false;
