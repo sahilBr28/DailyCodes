4fc0d5e937087dfa49a5eace67bdeaf8774a7cae
sahilBr28 <sahilkumar0092004@gmail.com>
2026-03-29 19:29:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -4,7 +4,7 @@ class Solution {
         int i = num.length()-1;
         while(i>=0){
             int sub = num.charAt(i)-'0';
-            if(sub%2==0){
+            if(sub%2!=0){
                 return num.substring(0, i+1);
             }
             i--;
