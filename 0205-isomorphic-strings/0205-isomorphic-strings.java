10a68ba6a42d3fb98f7e17f4c99b86290e6b793d
sahilBr28 <sahilkumar0092004@gmail.com>
2026-03-29 20:08:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,23 +2,22 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         int n = s.length();
         int m = t.length();
-        if(n != m){
-            return false;
-        }
+
+        if(n != m) return false;
+
         int[] count1 = new int[128];
         int[] count2 = new int[128];
+
         for(int i=0;i<n;i++){
             char ch1 = s.charAt(i);
             char ch2 = t.charAt(i);
 
-            if(count1[ch1] != 0 && count1[ch1] != ch2) return false;    
+            if(count1[ch1] != 0 && count1[ch1] != ch2) return false;
             if(count2[ch2] != 0 && count2[ch2] != ch1) return false;
 
             count1[ch1] = ch2;
             count2[ch2] = ch1;
-
         }
         return true;
-        
     }
 }
