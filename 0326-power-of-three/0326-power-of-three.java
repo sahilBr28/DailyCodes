99c8d28f6bc9580605cb218280f9496fa9613110
sahilBr28 <sahilkumar0092004@gmail.com>
2026-03-30 14:01:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<1){
-            return false;
-        }
-        else{
-            double c=(Math.log10(n))/(Math.log10(3));
-            int b=(int) c;
-            return (b==c);
-        }
+        return n > 0 && 1162261467%n == 0;
     }
 }
