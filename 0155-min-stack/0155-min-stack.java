b0bf46983c06f6ce1c9b5540fa131f6b8c1abfcf
sahilBr28 <sahilkumar0092004@gmail.com>
2026-04-06 21:34:40 +0530

    Time: 6 ms (14.29%), Space: 47 MB (56.43%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,38 +1,69 @@
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> minSt;
+    Stack<Long> st;
+    long min;
 
     public MinStack() {
         st = new Stack<>();
-        minSt = new Stack<>();    
+        min = Long.MAX_VALUE;    
     }
     
     public void push(int val) {
-        st.push(val);
-        if(minSt.size()==0 || val < minSt.peek()) minSt.push(val);
-        else minSt.push(minSt.peek());
+        if(st.size()==0) min = (long)val;
+        if((long)val >= min) st.push((long)val);
+        else{
+            st.push((long)val + ((long)val - min));
+            min = (long) val;
+        }
         
     }
     
     public void pop() {
+        if(st.peek() < min){
+            min = min + (min - st.peek());
+        }
         st.pop();
-        minSt.pop();
     }
     
     public int top() {
-        return st.peek();
+        long a = st.peek();
+        if(a < min){
+            return (int)min;
+        }
+        else  return (int) a;
     }
     
     public int getMin() {
-        return minSt.peek();
+        return (int) min;
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+
+// class MinStack {
+//     Stack<Integer> st;
+//     Stack<Integer> minSt;
+
+//     public MinStack() {
+//         st = new Stack<>();
+//         minSt = new Stack<>();    
+//     }
+    
+//     public void push(int val) {
+//         st.push(val);
+//         if(minSt.size()==0 || val < minSt.peek()) minSt.push(val);
+//         else minSt.push(minSt.peek());
+        
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//         minSt.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek();
+//     }
+    
+//     public int getMin() {
+//         return minSt.peek();
+//     }
+// }
