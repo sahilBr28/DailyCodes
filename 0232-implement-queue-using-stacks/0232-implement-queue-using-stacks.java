0d9d9ee06bf21251684ca9bc190fa8752f700713
sahilBr28 <sahilkumar0092004@gmail.com>
2026-04-08 12:22:49 +0530

    Time: 0 ms (100%), Space: 42.8 MB (47.51%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -5,43 +5,53 @@ class MyQueue {
     public MyQueue() {
         
     }
-    
-    public void push(int x) {
-        st.push(x);    
+
+    public void push(int x) { //Push at Bottom of the Stack
+        while(st.size()>0){
+            helper.push(st.pop());
+        }
+        st.push(x);
+        while(helper.size()>0){
+            st.push(helper.pop());
+        }    
     }
     
     public int pop() {
-        while(st.size()>1){
-            helper.push(st.pop());
-        }
-        int front = st.pop();
-        while(helper.size()>0){
-            st.push(helper.pop());
-        }
-        return front;
+        return st.pop();
     }
     
     public int peek() {
-        while(st.size()>1){
-            helper.push(st.pop());
-        }
-        int front = st.peek();
-        while(helper.size()>0){
-            st.push(helper.pop());
-        }
-        return front;
+        return st.peek();
     }
+    
+    // public void push(int x) {
+    //     st.push(x);    
+    // }
+    
+    // public int pop() {
+    //     while(st.size()>1){
+    //         helper.push(st.pop());
+    //     }
+    //     int front = st.pop();
+    //     while(helper.size()>0){
+    //         st.push(helper.pop());
+    //     }
+    //     return front;
+    // }
+    
+    // public int peek() {
+    //     while(st.size()>1){
+    //         helper.push(st.pop());
+    //     }
+    //     int front = st.peek();
+    //     while(helper.size()>0){
+    //         st.push(helper.pop());
+    //     }
+    //     return front;
+    // }
     
     public boolean empty() {
         return (st.size()==0);
     }
 }
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
