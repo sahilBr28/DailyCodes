5050cff60cbd6d6e16932aefdc13d54602127748
sahilBr28 <sahilkumar0092004@gmail.com>
2026-04-08 12:49:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -3,29 +3,47 @@ class MyStack {
     public MyStack() {
         
     }
-    
+
+
     public void push(int x) {
         q.add(x);
+        int n = q.size();
+        for(int i=1;i<=n-1;i++){
+            q.add(q.remove());
+        }
         
     }
     
     public int pop() {
-        int n = q.size();
-        for(int i=1;i<=n-1;i++){
-            q.add(q.remove());
-        }
         return q.remove();
     }
     
     public int top() {
-        int n = q.size();
-        for(int i=1;i<=n-1;i++){
-            q.add(q.remove());
-        }
-        int p = q.peek();
-        q.add(q.remove());
-        return p;
+        return q.peek();
     }
+    
+    // public void push(int x) {
+    //     q.add(x);
+        
+    // }
+    
+    // public int pop() {
+    //     int n = q.size();
+    //     for(int i=1;i<=n-1;i++){
+    //         q.add(q.remove());
+    //     }
+    //     return q.remove();
+    // }
+    
+    // public int top() {
+    //     int n = q.size();
+    //     for(int i=1;i<=n-1;i++){
+    //         q.add(q.remove());
+    //     }
+    //     int p = q.peek();
+    //     q.add(q.remove());
+    //     return p;
+    // }
     
     public boolean empty() {
         return (q.size()==0);
