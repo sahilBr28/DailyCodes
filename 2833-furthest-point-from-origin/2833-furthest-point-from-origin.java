34be99d999803fdbc67857ac1ac11936578c07a5
sahilBr28 <sahilkumar0092004@gmail.com>
2026-04-25 01:37:04 +0530

    Time: 1 ms (100%), Space: 43.6 MB (82.34%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int left = 0;
+        int right = 0;
+        int dash = 0;
+
+        for(int i=0;i<moves.length();i++){
+            char ch = moves.charAt(i);
+            if(ch=='L'){
+                left++;
+            }
+            if(ch=='R'){
+                right++;
+            }
+            if(ch=='_'){
+                dash++;
+            }
+        }
+        int origin = Math.abs(left-right)+dash;
+        return origin;
+    }
+}
