f003d447d3e2ac060591eda87963588f8e0e3b95
sahilBr28 <sahilkumar0092004@gmail.com>
2026-05-01 22:21:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -31,7 +31,7 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head.next;
 
-        while(fast==null && fast.next==null){
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
