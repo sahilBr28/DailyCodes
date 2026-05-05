6f91b46fa4ae9ed742b430423dcf068e374809bd
sahilBr28 <sahilkumar0092004@gmail.com>
2026-05-06 01:03:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,36 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public int Length(ListNode head){
+    public int length(ListNode head){
         int len = 0;
-        ListNode temp1 = head;
-        while(temp1 != null){
-            temp1 = temp1.next;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
             len++;
         }
         return len;
     }
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null){
+        if(head==null || head.next==null){
             return head;
         }
-        int n = Length(head);
+        int n = length(head);
         k %= n;
         if(k==0) return head;
+
         ListNode slow = head;
         ListNode fast = head;
-        for(int i=1;i<=k+1;i++){
+        for(int i=0;i<=k;i++){
             fast = fast.next;
         }
+
         while(fast != null){
             slow = slow.next;
             fast = fast.next;
         }
         ListNode newHead = slow.next;
         slow.next = null;
-        ListNode newTemp = newHead;
-        while(newTemp.next != null){
-            newTemp = newTemp.next;
+        ListNode temp = newHead;
+        while(temp.next != null){
+            temp = temp.next;
         }
-        newTemp.next = head;
+        temp.next = head;
         return newHead;
     }
 }
