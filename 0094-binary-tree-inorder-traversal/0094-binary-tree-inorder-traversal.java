f37a3fe3c1845cafe74a0a30db70445dae71adc7
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-05-12 09:40:36 +0530

    Time: 1 ms (6.19%), Space: 43.4 MB (14.33%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,28 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public void inOrder(TreeNode root, List<Integer> ans){
-        if(root==null) return;
-        inOrder(root.left, ans);
-        ans.add(root.val);
-        inOrder(root.right, ans);
-    }
+    // public void inOrder(TreeNode root, List<Integer> ans){
+    //     if(root==null) return;
+    //     inOrder(root.left, ans);
+    //     ans.add(root.val);
+    //     inOrder(root.right, ans);
+    // }
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> ans = new ArrayList<>();
+    //     inOrder(root, ans);
+    //     return ans;
+    // }
+
+    //Using Stack:
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        inOrder(root, ans);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(st.size()>0 || curr != null){
+            if(curr != null){
+                if(curr.left != null){
+                    st.push(curr);
+                    curr = curr.left;
+                }else{
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+
+            }else{
+                TreeNode top = st.pop();
+                ans.add(top.val);
+                curr = top.right;
+            }
+        }
         return ans;
     }
 }
