38d0d7ecd887f83516188d9e01eca910605d23fc
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-05-23 10:42:03 +0530

    Time: 0 ms (100%), Space: 43.8 MB (69.43%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,40 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public void preOrder(TreeNode root, ArrayList<TreeNode> arr){
-        if(root==null) return;
-
-        arr.add(root);
-        preOrder(root.left, arr);
-        preOrder(root.right, arr);
-    }
+    //Solve by Morris Traversal
     public void flatten(TreeNode root) {
-        if(root==null) return;
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        
-        preOrder(root, arr);
-        for(int i=0;i<arr.size()-1;i++){
-            TreeNode a = arr.get(i);
-            TreeNode b = arr.get(i+1);
-            a.left = null;
-            a.right = b;
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null){
+                    pred = pred.right;
+                }
+                pred.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+                curr = curr.right;
+            }
+            else{
+                curr = curr.right;
+            }
         }
-
-        TreeNode last = arr.get(arr.size()-1);
-        last.left = null;
-        last.right = null;
     }
 }
