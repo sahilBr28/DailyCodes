740378d2dd69a92e6aa621114c9142cb81952d03
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-06-19 21:24:27 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.89%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int[] dp;
+    public int fibo(int n){
+        if(n<=1) return n;
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        int ans = fibo(n-1) + fibo(n-2);
+        dp[n] = ans;
+        return ans;
+    }
+    public int fib(int n) {
+        dp = new int[n+1];
+        return fibo(n);
+    }
+}
