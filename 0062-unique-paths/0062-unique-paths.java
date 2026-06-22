8809e3965e8c14ddc62fd6a708b56a75dfd70b26
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-06-22 22:49:40 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.33%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int paths(int row, int col, Integer m, Integer n, int[][] dp){
+        if(row>=m || col>=n){
+            return 0;
+        }
+        if(row==m-1 && col==n-1){
+            return 1;
+        }
+        if(dp[row][col] != -1){
+            return dp[row][col];
+        }
+        int rightPath = paths(row, col+1, m, n, dp);
+        int downPath = paths(row+1, col, m, n, dp);
+        return dp[row][col] = rightPath + downPath;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return paths(0,0,m,n,dp);
+    }
+}
