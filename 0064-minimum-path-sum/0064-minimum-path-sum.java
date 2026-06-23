e305731d0bc61a40443decc640596aa799f7e73b
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-06-23 15:49:50 +0530

    Time: 2 ms (79.12%), Space: 50.3 MB (37.43%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,23 +1,21 @@
 class Solution {
-    public int minPathSum(int[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
+    public int pathSum(int[][] arr, int i, int j, int[][] dp){
+        if(i<0 || j<0) return Integer.MAX_VALUE;
+        if(i==0 && j==0) return arr[0][0];
+        if(dp[i][j] != -1) return dp[i][j];
+        int rightSum = pathSum(arr, i, j-1, dp);
+        int downSum = pathSum(arr, i-1, j, dp);
+        return dp[i][j] = arr[i][j] + Math.min(rightSum, downSum);
+    }
+    public int minPathSum(int[][] arr){
+        int n = arr.length, m = arr[0].length;
+        int[][] dp = new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 && j==0){
-                    continue;
-                }
-                else if(i==0){
-                    arr[i][j] += arr[i][j-1];
-                }
-                else if(j==0){
-                    arr[i][j] += arr[i-1][j];
-                }
-                else{
-                    arr[i][j] += Math.min(arr[i][j-1], arr[i-1][j]);
-                }
+                dp[i][j] = -1;
             }
         }
-        return arr[n-1][m-1];
+        dp[0][0] = arr[0][0];
+        return pathSum(arr, n-1,m-1,dp);
     }
 }
