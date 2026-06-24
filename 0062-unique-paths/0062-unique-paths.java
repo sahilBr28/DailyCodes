d83de9a82f5ecd03d78f97eed29630eda57c14a4
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-06-24 19:07:51 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,5 @@
 class Solution {
-     //DP after space optimization;
+    //Another method to optimize the space complexity;
     public int uniquePaths(int m, int n){
         int[][] dp = new int[2][n];
         for(int j=0;j<n;j++){
@@ -8,13 +8,17 @@ class Solution {
         }
         
         for(int i=1;i<=m-1;i++){
-            for(int j=1;j<n;j++){
-                dp[1][j] = dp[1][j-1] + dp[0][j];
+            if(i%2==0){
+                for(int j=1;j<n;j++){
+                    dp[1][j] = dp[1][j-1] + dp[0][j];
+                }
             }
-            for(int j=1;j<n;j++){
-                dp[0][j] = dp[1][j];
+            else{
+                for(int j=1;j<n;j++){
+                    dp[0][j] = dp[0][j-1] + dp[1][j];
+                }
             }
         }
-        return dp[1][n-1];
+        return Math.max(dp[1][n-1], dp[0][n-1]);
     }
 }
