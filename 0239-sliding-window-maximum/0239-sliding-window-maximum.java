a30a9c08290ad4206d4026f17713abcda6a21414
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-07-31 10:42:54 +0530

    Time: 27 ms (94.54%), Space: 143.5 MB (86.13%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+        if(n==0){
+            return ans;
+        }
+        Deque<Integer> dq = new ArrayDeque<Integer>();
+        int idx = 0;
+
+        while(idx < k){
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[idx]){
+                dq.pollLast();
+            }
+            dq.offerLast(idx);
+            idx++;
+        }
+        ans[0] = nums[dq.peekFirst()];
+        for(int i=1;i<n-k+1;i++){
+            if(!dq.isEmpty() && dq.peekFirst() <= (i-1)){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i+k-1]){
+                dq.pollLast();
+            }
+            dq.offerLast(i+k-1);
+            ans[i] = nums[dq.peekFirst()];
+        }
+        return ans;
+    }
+}
