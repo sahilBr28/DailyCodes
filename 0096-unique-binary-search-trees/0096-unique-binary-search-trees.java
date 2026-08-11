09393e466943af25fb94d8293be8dbce1c427d7e
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-08-11 23:42:20 +0530

    Time: 0 ms (100%), Space: 42 MB (69.65%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                int left = dp[root - 1];
+                int right = dp[nodes - root];
+
+                dp[nodes] += left * right;
+            }
+        }
+
+        return dp[n];
+    }
+}
