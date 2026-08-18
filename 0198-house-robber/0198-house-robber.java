52ea76f816b882a7127c2be8f8d190009a492dcf
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-08-18 15:32:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.9%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,16 @@
 class Solution {
-        //Tabulation Method of DP;
-
     public int rob(int[] nums) {
         int n = nums.length;
+        if(n==1) return nums[0];
         int[] dp = new int[n];
         dp[0] = nums[0];
-        if(n>1) dp[1] = Math.max(nums[0], nums[1]);
+        dp[1] = Math.max(nums[0], nums[1]);
 
         for(int i=2;i<n;i++){
             int take = nums[i] + dp[i-2];
             int skip = dp[i-1];
             dp[i] = Math.max(take, skip);
         }
-
         return dp[n-1];
     }
 }
