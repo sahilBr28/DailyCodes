8a470bcf3ffc08278af1cb0172a793f4b7dc790c
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-09-05 17:24:41 +0530

    Time: 25 ms (83.24%), Space: 44.5 MB (54%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,19 +3,19 @@ class Solution {
         int sqrt = (int)(Math.sqrt(n));
         return (sqrt*sqrt == n);
     }
-    public int minSquare(int n, int[] dp){
-        if(isPerfect(n)) return 1;
-        if(dp[n] != -1) return dp[n];
-        int min = Integer.MAX_VALUE;
-        for(int i=1;i*i<=n;i++){
-            int count = minSquare(i*i, dp) + minSquare(n-i*i, dp);
-            min = Math.min(min, count);
-        }
-        return dp[n] = min;
-    }
     public int numSquares(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return minSquare(n, dp);
+        for(int i=1;i<=n;i++){
+            if(isPerfect(i)) dp[i] = 1;
+            else{
+                int min = Integer.MAX_VALUE;
+                for(int j=1;j*j<=i;j++){
+                    int count = dp[j*j] + dp[i-j*j];
+                    min = Math.min(min, count);
+                }
+                dp[i] = min;
+            }
+        }
+        return dp[n];
     }
 }
