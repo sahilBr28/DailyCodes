025210bacb57ce96b85ddc78791946bf26c31c57
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-09-06 19:38:45 +0530

    Time: 633 ms (13.36%), Space: 43 MB (79.12%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,27 +1,14 @@
 class Solution {
-    private int countSubsets(int[] nums, int sum) {
-        int[] dp = new int[sum + 1];
-        dp[0] = 1;
-        
-        for (int num : nums) {
-            for (int j = sum; j >= num; j--) {
-                dp[j] += dp[j - num];
-            }
+    public int ways(int idx, int[] arr, int target){
+        if(idx==arr.length){
+            if(target==0) return 1;
+            else return 0;
         }
-        
-        return dp[sum];
+        int add = ways(idx+1,arr,target-arr[idx]);
+        int sub = ways(idx+1,arr,target+arr[idx]);
+        return add+sub;
     }
-    public int findTargetSumWays(int[] nums, int target) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        
-        if (totalSum < Math.abs(target) || (totalSum + target) % 2 != 0) {
-            return 0;
-        }
-        
-        int subsetSum = (totalSum + target) / 2;
-        return countSubsets(nums, subsetSum);
+    public int findTargetSumWays(int[] arr, int target) {
+        return ways(0,arr,target);
     }
 }
