acd124a16a5e9916437e13af9c12926edb9b02ce
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-09-07 23:41:44 +0530

    Time: 36 ms (11.51%), Space: 47.8 MB (5.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,24 @@
 class Solution {
-    public long coinCount(int idx, int[] coins, int amount, long[][] dp){
+    //Memoization Method
+    public long coinWay(int idx, int[] coins, int amount, long[][] dp){
         if(idx==coins.length){
             if(amount==0) return 0;
             else return Integer.MAX_VALUE;
         }
         if(dp[idx][amount] != -1) return dp[idx][amount];
-        long skip = coinCount(idx+1,coins,amount,dp);
-        if(amount-coins[idx]<0) return dp[idx][amount]= skip;
-        long take = 1+coinCount(idx,coins,amount-coins[idx],dp);
-        return dp[idx][amount]=Math.min(take, skip);
+        long skip = coinWay(idx+1,coins,amount,dp);
+        if(amount-coins[idx]<0) return dp[idx][amount] = skip;
+        long take = 1 + coinWay(idx,coins,amount-coins[idx],dp);
+        return dp[idx][amount] = Math.min(take,skip);
     }
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange(int[] coins, int amount){
         long[][] dp = new long[coins.length][amount+1];
-        for(int i=0;i<dp.length;i++){
+        for(int i=0;i< dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
                 dp[i][j] = -1;
             }
         }
-        int ans = (int)coinCount(0,coins,amount,dp);
+        int ans = (int)coinWay(0,coins,amount,dp);
         if(ans==Integer.MAX_VALUE) return -1;
         return ans;
     }
