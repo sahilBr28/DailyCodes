d3e93009f027c0157b56f4b48bf70f8c8d38815c
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-09-08 00:03:23 +0530

    Time: 10 ms (52.3%), Space: 47.1 MB (14.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,25 @@
 class Solution {
-    //Recursive Code
-    public int ways(int idx, int[] arr, int target){
+    //Memoization Method
+    static int sum;
+    public int ways(int idx, int[] arr, int res, int target, int[][] dp){
         if(idx==arr.length){
-            if(target==0) return 1;
+            if(res==target) return 1;
             else return 0;
         }
-        int add = ways(idx+1,arr,target-arr[idx]);
-        int sub = ways(idx+1,arr,target+arr[idx]);
-        return add+sub;
+        if(dp[idx][res+sum] != -1) return dp[idx][res+sum];
+        int add = ways(idx+1,arr,res+arr[idx],target,dp);
+        int sub = ways(idx+1,arr,res-arr[idx],target,dp);
+        return dp[idx][res+sum] = add+sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return ways(0,nums,target);
+        sum = 0;
+        for(int ele : nums) sum += ele;
+        int[][] dp = new int[nums.length][2*sum+1];
+        for(int i=0;i< dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return ways(0,nums,0,target,dp);
     }
 }
