fb663d99c591b706ce78b848d34cd8850a252dcf
Sahil Kumar <sahilkumar0092004@gmail.com>
2026-09-07 19:59:18 +0530

    Time: 79 ms (33.02%), Space: 75.9 MB (14.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,34 +1,24 @@
 class Solution {
-        //Memoization Method
-    static boolean subset(int idx, int[] arr, int target, int[][] dp){
-        if(idx==arr.length){
-            if(target==0) return true;
-            else return false;
-        }
-        if(dp[idx][target] != -1) return (dp[idx][target] == 1);
-        boolean ans = false;
-        boolean skip = subset(idx+1,arr,target,dp);
-        if(target-arr[idx]<0) ans = skip;
-        else{
-            boolean take = subset(idx+1,arr,target-arr[idx],dp);
-            ans = take || skip;
-        }
-        if(ans) dp[idx][target] = 1;
-        else dp[idx][target] = 0;
-        return ans;
-    }
-    public boolean canPartition(int[] nums) {
+    //Tabulation Method
+    public boolean canPartition(int[] nums){
         int sum = 0;
-        for(int ele : nums) sum+=ele;
+        for(int ele:nums) sum+=ele;
         if(sum%2 != 0) return false;
         int target = sum/2;
-
-        int[][] dp = new int[nums.length][sum+1];
-        for(int i=0;i< dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j] = -1;
+        
+        int[][] dp = new int[nums.length][target+1];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<target+1;j++){
+                boolean ans = false;
+                boolean skip = (i>0) ? (dp[i-1][j]==1) : (j==0);
+                if(j-nums[i]<0) ans = skip;
+                else{
+                    boolean take = (i>0) ? (dp[i-1][j-nums[i]]==1) : (j==nums[i]);
+                    ans = take || skip;
+                }
+                dp[i][j] = (ans) ? 1:0;
             }
         }
-        return subset(0,nums,target,dp);
+        return (dp[nums.length-1][target]==1);
     }
 }
